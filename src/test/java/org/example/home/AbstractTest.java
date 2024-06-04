00864ca9832a0ce2f0db943970d0b1edeb17a7a1
Volha Vybarnaya <olyaviborn@gmail.com>
2024-06-04 17:17:14 +0300

    Initial commit

---
+++ src/test/java/org/example/home/AbstractTest.java
@@ -0,0 +1,20 @@
+package org.example.home;
+import org.example.home.home.Door;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractTest  {
+
+    static List<Door> doors;
+
+    @BeforeEach
+    void initDoors () {
+        doors = new ArrayList<>();
+        doors.add(new Door(true));
+        doors.add(new Door(false));
+        doors.add(new Door(false));
+    }
+
+}
